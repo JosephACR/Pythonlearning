00bd9f6b070861c124d5f5be05ed867f4e07ad72
davidmedinaskate <davidmedinaskate@gmail.com>
2015-10-29 14:30:21 -0600

    mamadas

--- hola/archivo.py
+++ hola/archivo.py
@@ -1,7 +1,8 @@
 def lines(nombreArchivo,N):
- try:
-  f = open(nombreArchivo,"r")
-  l = f.readlines()
-  return l[:N]
- except:
-  return("error")
+    try:
+        f = open(nombreArchivo,"r")
+        l = f.readlines()
+        return l[:N]
+    except:
+        return("error")
+    
--- hola/archivodavid.py
+++ hola/archivodavid.py
@@ -1,4 +1,11 @@
-
-  f = open(pt-data1.scv)
-  l = f.readlines()
-  print l[:118]
+def lines(nombreArchivo,N):
+    try:
+        f = open(nombreArchivo,"r")
+        l = f.readlines()
+        return l[:N]
+    except:
+        return("error")
+simbolo= str(input("introduzca el simbolo entre comillas"))
+for i in range(1,118):
+    serie= (lines('pt-data1.csv',i).split('')
+    print serie[2]
