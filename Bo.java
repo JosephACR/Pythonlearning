62c134017e5eae45cce82c057721569142cc4d42
ismaelom <fismat@iMac3.local>
2015-10-29 11:59:57 -0600

    ya

---
+++ Bo.java
@@ -0,0 +1,5 @@
+class Bo{
+  public static void main(String args[]){
+    System.out.print("hola mundo");
+  }
+}
---
+++ ejemplo.java
@@ -0,0 +1,5 @@
+class ejemplo{
+  public static void main(String args[]){
+    System.out.print("hola mundo");
+  }
+}
