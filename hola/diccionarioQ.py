90264995b1002130e08e8fb9d7f9f7f7cec6da64
Joseph <coronadorojasjoseantonio@gmail.com>
2015-10-29 13:21:26 -0600

    al fin algo

--- hola/diccionarioQ.py
+++ hola/diccionarioQ.py
@@ -1,13 +1,7 @@
-## Las ideas no fluyen
-## Sabemos lo que tenemos que hacer
-def Dicc quimico (simbolo,nombre,masaatomica):
-    N = 118
-    def lines(nombreArchivo,N):
- try:
-  f = open(nombreArchivo,"r")
-  l = f.readlines()
-  Data =l[:N]
-  simbolo = Data[2]
-  nombre = Data[3]
-  if simbolo in Data[2]:
-      return
+def lines(nombreArchivo,N):
+    try:
+        f = open(nombreArchivo,"r")
+        l = f.readlines()
+        return l[N-1:N]
+    except:
+        return("error")
